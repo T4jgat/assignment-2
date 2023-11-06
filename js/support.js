679905bd7c9382ab6296eabad9e59a82022d7943
t4jgat <tmtmtmt1423@gmail.com>
2023-11-06 14:56:09 +0600

    add sounds

--- js/support.js
+++ js/support.js
@@ -1,4 +1,8 @@
 function sendFeedback() {
-    location.reload(true)
-    alert("Customer service has received your message!");
+    const sound = new Audio();
+    sound.src = "./js/achievement-bell-600.wav";
+    sound.play();
+
+    setTimeout(() => alert("Customer service has received your message!"), 1000);
 }
+
